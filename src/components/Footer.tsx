3b2e719eb699a6a0510158955aca326b1010bdbf
AnshKr2004 <anshr795@gmail.com>
2024-09-09 01:13:06 +0530

    committed by Ansh

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,65 +3,67 @@ import { Facebook, Youtube, Dribbble, Instagram, Linkedin } from 'lucide-react'
 const Footer = () => {
   return (
     <>
-        <footer className="bg-gray-950 text-gray-400 py-12 px-4 md:px-6 w-screen">
-        <div className="max-w-6xl border border-gray-600 rounded-md p-4 mx-auto">
+      <footer className="bg-gray-950 text-gray-400 py-12 px-4 md:px-6 w-screen">
+        <div className="max-w-6xl rounded-md p-4 mx-auto">
           <div className="grid grid-cols-1  md:grid-cols-2 gap-8 mb-12">
-            <div className="space-y-4  border-gray-600  ">
-              <span className="text-purple-500  uppercase text-sm font-semibold">Carrers</span>
-              <h2 className="text-2xl font-bold text-white">Join to help businesses grow</h2>
-              <button className="text-white border-purple-500 hover:bg-purple-500">
+            <div className="space-y-4 bg-[#13151a] px-16 py-20 rounded-xl">
+              <span className="text-purple-500  uppercase font-semibold text-base">Carrers</span>
+              <h2 className="text-4xl font-bold text-white pb-14">Join to help businesses grow</h2>
+              <button className="text-white text-base border-purple-500 hover:bg-purple-500">
                 Explore Careers <span className="ml-2">→</span>
               </button>
             </div>
-            <div className="space-y-4">
-              <span className="text-purple-500 uppercase text-sm font-semibold">Blog</span>
-              <h2 className="text-2xl font-bold text-white">Tips, and trends for your future job</h2>
-              <button className="text-white border-purple-500 hover:bg-purple-500">
+            <div className="space-y-4 bg-[#13151a] rounded-xl px-16 py-20">
+              <span className="text-purple-500 uppercase text-base font-semibold">Blog</span>
+              <h2 className="text-4xl font-bold text-white pb-14">Tips, and trends for your future job</h2>
+              <button className="text-white text-base border-purple-500 hover:bg-purple-500">
                 Explore Blog <span className="ml-2">→</span>
               </button>
             </div>
           </div>
 
-          <div className="mb-12 grid text-center">
-            <h3 className="text-xl font-semibold text-white mb-4">Subscribe</h3>
-            <p className="mb-4">Stay current with all thinks JobBoard</p>
-            <form className="flex space-x-2">
-              <input type="email" placeholder="Email" className="bg-gray-900 border-gray-800 text-white flex-grow rounded-full px-4" />
-              <button type="submit" className="bg-purple-500 text-white hover:bg-purple-600 rounded-full p-2">
-                Subscribe
-              </button>
-            </form>
-          </div>
+          <div className='md:flex gap-36'>
+            <div className="mb-12 bg-[#13151A] rounded-3xl pr-12 pl-10 py-11">
+              <h3 className="text-xl font-semibold text-white">Subscribe</h3>
+              <p className="mb-8">Stay current with all thinks JobBoard</p>
+              <div className="flex space-x-2">
+                <input type="email" placeholder="Email" className="bg-gray-900 border-gray-800 text-white flex-grow rounded-full px-4 w-0" />
+                <button type="submit" className="bg-purple-500 text-white hover:bg-purple-600 rounded-full p-2">
+                  Subscribe
+                </button>
+              </div>
+            </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 text-center">
-            <div>
-              <h3 className="text-white font-semibold mb-4">COMPANY</h3>
-              <ul className="space-y-2">
-                <li><a href="#" className="hover:text-white">About</a></li>
-                <li><a href="#" className="hover:text-white">Our Team</a></li>
-                <li><a href="#" className="hover:text-white">Jobs</a></li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-white font-semibold mb-4">RESOURCES</h3>
-              <ul className="space-y-2">
-                <li><a href="#" className="hover:text-white">News</a></li>
-                <li><a href="#" className="hover:text-white">FAQs</a></li>
-                <li><a href="#" className="hover:text-white">Candidates</a></li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-white font-semibold mb-4">CONTACTS</h3>
-              <ul className="space-y-2">
-                <li><a href="#" className="hover:text-white">CV Post</a></li>
-                <li><a href="#" className="hover:text-white">Job Post</a></li>
-                <li><a href="#" className="hover:text-white">Contact Us</a></li>
-              </ul>
+            <div className="grid grid-cols-3 md:gap-32 mb-12 text-center">
+              <div>
+                <h3 className="text-white font-semibold mb-4">COMPANY</h3>
+                <ul className="space-y-2">
+                  <li><a href="#" className="hover:text-white">About</a></li>
+                  <li><a href="#" className="hover:text-white">Our Team</a></li>
+                  <li><a href="#" className="hover:text-white">Jobs</a></li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-white font-semibold mb-4">RESOURCES</h3>
+                <ul className="space-y-2">
+                  <li><a href="#" className="hover:text-white">News</a></li>
+                  <li><a href="#" className="hover:text-white">FAQs</a></li>
+                  <li><a href="#" className="hover:text-white">Candidates</a></li>
+                </ul>
+              </div>
+              <div>
+                <h3 className="text-white font-semibold mb-4">CONTACTS</h3>
+                <ul className="space-y-2">
+                  <li><a href="#" className="hover:text-white">CV Post</a></li>
+                  <li><a href="#" className="hover:text-white">Job Post</a></li>
+                  <li><a href="#" className="hover:text-white">Contact Us</a></li>
+                </ul>
+              </div>
             </div>
           </div>
 
           <div className="flex flex-col md:flex-row justify-between items-center pt-8 border-t border-gray-800">
-            <div className="mb-4 md:mb-0">
+            <div className="mb-8 md:mb-0">
               <svg className="h-8 w-auto" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect width="100" height="100" rx="10" fill="white" />
                 <path d="M20 80V20H40V80H20ZM60 80V20H80V80H60Z" fill="black" />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,6 @@ import Discover from "../components/Discover"
 import Footer from "../components/Footer"
 import GetStarted from "../components/GetStarted"
 import Hero from "../components/Hero"
-import Jobboard from "../components/Jobboard"
 import Nav from "../components/Nav"
 
 const Home = () => {
@@ -10,7 +9,6 @@ const Home = () => {
     <div className=" overflow-hidden">
         <Nav/>
         <Hero/>
-        <Jobboard/>
         <GetStarted/>
         <Discover/>
         <Footer/>
